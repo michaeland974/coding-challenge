ca29c9591fa0b7e0642ec91b32a8e7e9699c7601
michaeland974 <micanderson29@gmail.com>
2023-11-05 08:04:58 -0600

    feat: custom element: nav bar added

---
+++ src/custom-elements/NavBar/NavBar.js
@@ -0,0 +1,39 @@
+import { CustomElementFactory as Factory } from "../factory";
+
+const NavBar = Factory.createElement(`
+  <ul class="nav-list">
+    <li>
+      <a href="#home">
+        <span class="icon" role="presentation" data-icon="home"></span>
+        Home
+      </a>
+    </li>
+    <li>
+      <a href="#records">
+        <span class="icon" role="presentation" data-icon="records"></span>
+        Records
+      </a>
+    </li>
+    <li>
+      <a href="#music">
+        <span class="icon" role="presentation" data-icon="music"></span>
+        Music
+      </a>
+    </li>
+    <li>
+      <a href="#artists">
+        <span class="icon" role="presentation" data-icon="artists"></span>
+        Artists
+      </a>
+    </li>
+    <li>
+      <a href="#concerts">
+        <span class="icon" role="presentation" data-icon="concerts"></span>
+        Concerts
+      </a>
+    </li>
+  </ul>
+  <button class="sign-up">Sign Up</button>
+`);
+
+customElements.define("nav-bar", NavBar);
