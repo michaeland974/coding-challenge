857c16f3f01fdf38bfb4d8742fc6c8d8664c8f63
michaeland974 <micanderson29@gmail.com>
2023-11-05 16:22:05 -0600

    feat: mobile nav bar added

---
+++ src/custom-elements/NavBar/MobileNavBar.js
@@ -0,0 +1,25 @@
+import { CustomElementFactory as Factory } from "../factory";
+
+const MobileNavBar = Factory.createElement(`
+  <nav-bar id="mobile-nav-bar" 
+           role="navigation" 
+           aria-expanded="false">
+    <button id="bar-toggle" 
+            aria-controls="mobile-nav-bar"
+            aria-expanded="false">
+      <span class="line"></span>
+      <span class="line"></span>
+      <span class="line"></span>
+    </button>
+  </nav-bar>
+`);
+
+customElements.define("mobile-nav-bar", MobileNavBar);
+
+const toggle = document.querySelector("#bar-toggle");
+const navLinks = document.querySelector("#mobile-nav-bar .link-group");
+
+toggle.addEventListener("click", () => {
+  toggle.ariaExpanded = (toggle.ariaExpanded !== "true");
+  navLinks.ariaExpanded = (navLinks.ariaExpanded !== "true");
+});
--- src/custom-elements/NavBar/NavBar.js
+++ src/custom-elements/NavBar/NavBar.js
@@ -1,39 +1,41 @@
 import { CustomElementFactory as Factory } from "../factory";
 
 const NavBar = Factory.createElement(`
-  <ul class="nav-list">
-    <li>
-      <a href="#home">
-        <span class="icon" role="presentation" data-icon="home"></span>
-        Home
-      </a>
-    </li>
-    <li>
-      <a href="#records">
-        <span class="icon" role="presentation" data-icon="records"></span>
-        Records
-      </a>
-    </li>
-    <li>
-      <a href="#music">
-        <span class="icon" role="presentation" data-icon="music"></span>
-        Music
-      </a>
-    </li>
-    <li>
-      <a href="#artists">
-        <span class="icon" role="presentation" data-icon="artists"></span>
-        Artists
-      </a>
-    </li>
-    <li>
-      <a href="#concerts">
-        <span class="icon" role="presentation" data-icon="concerts"></span>
-        Concerts
-      </a>
-    </li>
-  </ul>
-  <button class="sign-up">Sign Up</button>
+  <div class="link-group">
+    <ul class="nav-list">
+      <li>
+        <a href="#home">
+          <span class="icon" role="presentation" data-icon="home"></span>
+          Home
+        </a>
+      </li>
+      <li>
+        <a href="#records">
+          <span class="icon" role="presentation" data-icon="records"></span>
+          Records
+        </a>
+      </li>
+      <li>
+        <a href="#music">
+          <span class="icon" role="presentation" data-icon="music"></span>
+          Music
+        </a>
+      </li>
+      <li>
+        <a href="#artists">
+          <span class="icon" role="presentation" data-icon="artists"></span>
+          Artists
+        </a>
+      </li>
+      <li>
+        <a href="#concerts">
+          <span class="icon" role="presentation" data-icon="concerts"></span>
+          Concerts
+        </a>
+      </li>
+    </ul>
+    <button class="sign-up action-button">Sign Up</button>
+  </div>
 `);
 
 customElements.define("nav-bar", NavBar);
