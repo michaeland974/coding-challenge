9e1356276fbadf941da3e60db7d15f9f0314a510
michaeland974 <micanderson29@gmail.com>
2023-11-06 16:36:26 -0600

    fix: span tag added for centering

--- src/custom-elements/Sitemap/Address.js
+++ src/custom-elements/Sitemap/Address.js
@@ -2,7 +2,8 @@ import { CustomElementFactory as Factory } from "../factory";
 
 const Address = Factory.createElement(`
   <address>
-    <span>8521 W Fairfax Rd Los Angeles, CA</span>
+    <span>8521 W Fairfax Rd</span>
+    <span>Los Angeles, CA</span>
     <div>
       <span>990210</span>
       <a href="tel:+19095054302">(909) 505 4302</a>
