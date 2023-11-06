11a6760ef9a31f3a99df0bb19ae77c62545e503c
michaeland974 <micanderson29@gmail.com>
2023-11-06 16:34:43 -0600

    feat: footer added

--- src/entry.js
+++ src/entry.js
@@ -1,3 +1,9 @@
 import "./custom-elements/factory.js";
 import "./custom-elements/NavBar/NavBar.js";
 import "./custom-elements/NavBar/MobileNavBar.js";
+import "./custom-elements/Sitemap/SocialBar.js";
+import "./custom-elements/Sitemap/Address.js";
+import "./custom-elements/Sitemap/Columns/Artists.js";
+import "./custom-elements/Sitemap/Columns/Explore.js";
+import "./custom-elements/Sitemap/Columns/Records.js";
+import "./custom-elements/Sitemap/Columns/Music.js";
