5e061687abbcec89d4cc2d9301041a6f6a847387
michaeland974 <micanderson29@gmail.com>
2023-11-06 20:00:04 -0600

    fix: span added for flex positioning

--- src/custom-elements/Sitemap/Address.js
+++ src/custom-elements/Sitemap/Address.js
@@ -2,8 +2,10 @@ import { CustomElementFactory as Factory } from "../factory";
 
 const Address = Factory.createElement(`
   <address>
-    <span>8521 W Fairfax Rd</span>
-    <span>Los Angeles, CA</span>
+    <div>
+      <span>8521 W Fairfax Rd</span>
+      <span>Los Angeles, CA</span>
+    </div>
     <div>
       <span>990210</span>
       <a href="tel:+19095054302">(909) 505 4302</a>
